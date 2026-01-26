84fd58dfe384382e467953ba3bc7b63361b2473a
Sunny Gupta <guptakamlesh469@gmail.com>
2026-01-26 23:40:44 +0530

    Made data folder and product.js in it. Also Object Array from Fashion Jewellery/page.jsx

--- okhai/src/components/collections/Accessories/FashoinJewellery/page.jsx
+++ okhai/src/components/collections/Accessories/FashoinJewellery/page.jsx
@@ -1,36 +1,7 @@
 import React from 'react';
 
 const FashoinJewellery = () => {
-    const fashionJewelleryProduct = [
-        {
-            _id: 'fjEarring',
-            name: 'Samoolam Handmade Crescent Moon Beige, Brown Earrings Beige',
-            price: 650,
-            image: ['https://okhai.org/cdn/shop/files/EAR064-A_1.jpg?v=1698667378&width=750', 'https://okhai.org/cdn/shop/files/EAR064-A.jpg?v=1698667378&width=750', 'https://okhai.org/cdn/shop/files/EAR064-A_2.jpg?v=1698667376&width=750'],
-            isNew: true,
-            description: 'Discover our captivating collection of contemporary minimalist earrings designed with a minimalist touch to elevate your everyday style. These versatile pieces are the perfect pairing for both Indian and western wear, adding a head-turning allure to any occasion. Embrace the simplicity and sophistication of our timeless designs, crafted to complement your fashion sensibilities and create a lasting impression wherever you go. This piece is hand-crafted by our women artisans in Gaya to be lightweight, easy to maintain and bring out the chic in you.',
-            washCare: 'Cotton Apparel - Hand wash separately. Silk & Madhubani - Dry clean only. Baskets & Show Pieces - Clean with a wet cloth. For any other product - Please find the wash care in the product description.',
-            skipping: 'Free shipping within India on orders valued above INR 4999. Shipping out of India is as per weight calculation at checkout.',
-            giftWrapAvailable: 'To gift wrap order, select the gift wrap check box on the cart page during checkout',
-            return30Days: 'Okhai Apparels - 30 Days Free Return. On SALE - No Return Refund. Please Note - Custom Made Handicrafts - No Returns No Refund as mentioned in the product description.',
-            estimatedDeliveryTime: 'Okhai Apparel - 4-6 Working Days. Custom Handicrafts - 15 to 30 days as artisans make it after they receive the order.',
-            colors: 'The colours seen in the image may vary from the actual product due to different computer screen resolutions and displays. Dark colours may look lighter in direct sunlight.'
-        },
-        {
-            _id: 'fjNecklaces',
-            name: 'Samoolam Aadya Necklace ~ Gold',
-            price: 1650,
-            image: ['https://okhai.org/cdn/shop/products/NEK065-A-1.jpg?v=1642849789&width=750', 'https://okhai.org/cdn/shop/products/NEK065-A-2.jpg?v=1642849789&width=750', 'https://okhai.org/cdn/shop/products/NEK065-A-3.jpg?v=1642849789&width=750'],
-            isNew: true,
-            description: 'Product Detail: Let this three-layered necklace with metallic crochet beads and chain add a dash of drama to your looks. Contrast it with a solid-coloured ensemble or match it with a printed separate.',
-            washCare: 'Cotton Apparel - Hand wash separately. Silk & Madhubani - Dry clean only. Baskets & Show Pieces - Clean with a wet cloth. For any other product - Please find the wash care in the product description.',
-            skipping: 'Free shipping within India on orders valued above INR 4999. Shipping out of India is as per weight calculation at checkout.',
-            giftWrapAvailable: 'To gift wrap order, select the gift wrap check box on the cart page during checkout',
-            return30Days: 'Okhai Apparels - 30 Days Free Return. On SALE - No Return Refund. Please Note - Custom Made Handicrafts - No Returns No Refund as mentioned in the product description.',
-            estimatedDeliveryTime: 'Okhai Apparel - 4-6 Working Days. Custom Handicrafts - 15 to 30 days as artisans make it after they receive the order.',
-            colors: 'The colours seen in the image may vary from the actual product due to different computer screen resolutions and displays. Dark colours may look lighter in direct sunlight.'
-        }
-    ]
+    
     return (
         <>
             <div>
---
+++ okhai/src/components/collections/Accessories/data/product.js
@@ -0,0 +1,63 @@
+export default allAccessoriesProducts = {
+    fashionJewelleryProduct: [
+        {
+            _id: 'fjEarring',
+            name: 'Samoolam Handmade Crescent Moon Beige, Brown Earrings Beige',
+            price: 650,
+            image: ['https://okhai.org/cdn/shop/files/EAR064-A_1.jpg?v=1698667378&width=750', 'https://okhai.org/cdn/shop/files/EAR064-A.jpg?v=1698667378&width=750', 'https://okhai.org/cdn/shop/files/EAR064-A_2.jpg?v=1698667376&width=750'],
+            isNew: true,
+            description: 'Discover our captivating collection of contemporary minimalist earrings designed with a minimalist touch to elevate your everyday style. These versatile pieces are the perfect pairing for both Indian and western wear, adding a head-turning allure to any occasion. Embrace the simplicity and sophistication of our timeless designs, crafted to complement your fashion sensibilities and create a lasting impression wherever you go. This piece is hand-crafted by our women artisans in Gaya to be lightweight, easy to maintain and bring out the chic in you.',
+            washCare: 'Cotton Apparel - Hand wash separately. Silk & Madhubani - Dry clean only. Baskets & Show Pieces - Clean with a wet cloth. For any other product - Please find the wash care in the product description.',
+            skipping: 'Free shipping within India on orders valued above INR 4999. Shipping out of India is as per weight calculation at checkout.',
+            giftWrapAvailable: 'To gift wrap order, select the gift wrap check box on the cart page during checkout',
+            return30Days: 'Okhai Apparels - 30 Days Free Return. On SALE - No Return Refund. Please Note - Custom Made Handicrafts - No Returns No Refund as mentioned in the product description.',
+            estimatedDeliveryTime: 'Okhai Apparel - 4-6 Working Days. Custom Handicrafts - 15 to 30 days as artisans make it after they receive the order.',
+            colors: 'The colours seen in the image may vary from the actual product due to different computer screen resolutions and displays. Dark colours may look lighter in direct sunlight.'
+        },
+        {
+            _id: 'fjNecklaces',
+            name: 'Samoolam Aadya Necklace ~ Gold',
+            price: 1650,
+            image: ['https://okhai.org/cdn/shop/products/NEK065-A-1.jpg?v=1642849789&width=750', 'https://okhai.org/cdn/shop/products/NEK065-A-2.jpg?v=1642849789&width=750', 'https://okhai.org/cdn/shop/products/NEK065-A-3.jpg?v=1642849789&width=750'],
+            isNew: true,
+            description: 'Product Detail: Let this three-layered necklace with metallic crochet beads and chain add a dash of drama to your looks. Contrast it with a solid-coloured ensemble or match it with a printed separate.',
+            washCare: 'Cotton Apparel - Hand wash separately. Silk & Madhubani - Dry clean only. Baskets & Show Pieces - Clean with a wet cloth. For any other product - Please find the wash care in the product description.',
+            skipping: 'Free shipping within India on orders valued above INR 4999. Shipping out of India is as per weight calculation at checkout.',
+            giftWrapAvailable: 'To gift wrap order, select the gift wrap check box on the cart page during checkout',
+            return30Days: 'Okhai Apparels - 30 Days Free Return. On SALE - No Return Refund. Please Note - Custom Made Handicrafts - No Returns No Refund as mentioned in the product description.',
+            estimatedDeliveryTime: 'Okhai Apparel - 4-6 Working Days. Custom Handicrafts - 15 to 30 days as artisans make it after they receive the order.',
+            colors: 'The colours seen in the image may vary from the actual product due to different computer screen resolutions and displays. Dark colours may look lighter in direct sunlight.'
+        }
+    ],
+
+    bagsProduct: [
+        {
+            _id: 'toteBags',
+            name: 'Okhai "London" Hand-Embroidered Pure Cotton Tote Bag',
+            price: 1875,
+            image: ['https://okhai.org/cdn/shop/files/BG24929430_1.webp?v=1754286650&width=750', 'https://okhai.org/cdn/shop/files/BG24929430_2.webp?v=1754286651&width=1946', 'https://okhai.org/cdn/shop/files/BG24929430_3.webp?v=1754286651&width=750', 'https://okhai.org/cdn/shop/files/BG24929430_4.webp?v=1754286651&width=750', 'https://okhai.org/cdn/shop/files/BG24929430_5.webp?v=1754286651&width=750', 'https://okhai.org/cdn/shop/files/BG24929430_6.webp?v=1754286651&width=750', 'https://okhai.org/cdn/shop/files/BG24929430_7.webp?v=1754286651&width=750'],
+            isNew: true,
+            description: 'Step into London with every step you take, carrying this intricately hand-embroidered tote. Crafted from pure cotton, its body features a beautifully recreated map of the city, with the iconic Thames winding through and a single charming red double-decker bus—all brought to life by our artisans skilled handiwork. A convenient pocket ensures it’s as practical as it is stylish, making it the ideal on-the-go companion. Its neutral tones blend seamlessly with any outfit, offering timeless versatility. And as you admire the detailed craftsmanship, heres a fun challenge: can you spot Big Ben?',
+            washCare: 'Cotton Apparel - Hand wash separately. Silk & Madhubani - Dry clean only. Baskets & Show Pieces - Clean with a wet cloth. For any other product - Please find the wash care in the product description.',
+            skipping: 'Free shipping within India on orders valued above INR 4999. Shipping out of India is as per weight calculation at checkout.',
+            giftWrapAvailable: 'To gift wrap order, select the gift wrap check box on the cart page during checkout',
+            return30Days: 'Okhai Apparels - 30 Days Free Return. On SALE - No Return Refund. Please Note - Custom Made Handicrafts - No Returns No Refund as mentioned in the product description.',
+            estimatedDeliveryTime: 'Okhai Apparel - 4-6 Working Days. Custom Handicrafts - 15 to 30 days as artisans make it after they receive the order.',
+            colors: 'The colours seen in the image may vary from the actual product due to different computer screen resolutions and displays. Dark colours may look lighter in direct sunlight.'
+        },
+        {
+            _id: 'beachBags',
+            name: 'House Of Macrame "Fringe Fest" Handcrafted Clutch Bag',
+            price: 1850,
+            image: [],
+            isNew: true,
+            description: 'Fashionable and sustainable, the Fringe Fest clutch is the perfect companion for outings with your woke friends. Be an environment companion and let your fashion do the talkingIt has a small pouch with a zipper to keep your tiny trinkets safely inside.House of Macrame is a small group of woman artisans based in a small town of Banswara, in Rajasthan. Our motto is to teach this craft to as many women as possible and provide them with an extra source of income while creating beautiful products for our beloved patrons.',
+            washCare: 'Cotton Apparel - Hand wash separately. Silk & Madhubani - Dry clean only. Baskets & Show Pieces - Clean with a wet cloth. For any other product - Please find the wash care in the product description.',
+            skipping: 'Free shipping within India on orders valued above INR 4999. Shipping out of India is as per weight calculation at checkout.',
+            giftWrapAvailable: 'To gift wrap order, select the gift wrap check box on the cart page during checkout',
+            return30Days: 'Okhai Apparels - 30 Days Free Return. On SALE - No Return Refund. Please Note - Custom Made Handicrafts - No Returns No Refund as mentioned in the product description.',
+            estimatedDeliveryTime: 'Okhai Apparel - 4-6 Working Days. Custom Handicrafts - 15 to 30 days as artisans make it after they receive the order.',
+            colors: 'The colours seen in the image may vary from the actual product due to different computer screen resolutions and displays. Dark colours may look lighter in direct sunlight.',
+        }
+    ]
+};
