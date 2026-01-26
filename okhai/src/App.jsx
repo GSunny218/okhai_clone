3cb183b86ef1ba35991ac94f6a4f5ad7b2867f39
Sunny Gupta <guptakamlesh469@gmail.com>
2026-01-26 23:03:47 +0530

    Started making Accessories part

--- okhai/src/App.jsx
+++ okhai/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/ui/Navbar.jsx';
 import Footer from './components/ui/Footer.jsx';
 import Main from './components/ui/Main.jsx';
 import AllClothing from './components/collections/Clothing/products/women/all-clothing.jsx';
+import FashoinJewellery from './components/collections/Accessories/FashoinJewellery/page.jsx';
 
 function App() {
 
@@ -11,10 +12,12 @@ function App() {
     <>
       <Navbar />
       {/* <div className='absolute top-48 left-0'>
-      <AllClothing />
+      
       </div> */}
       <div className='absolute top-48 left-0'>
-        <Main />
+        {/* <Main /> */}
+        {/* <AllClothing /> */}
+        <FashoinJewellery />
         <Footer />
       </div>
     </>
